f63cb485c9ca4d8c6c0f65ff36b321c5692e0eaa
Kimyeonjin1230 <kyj9750322@naver.com>
2024-04-30 20:24:43 +0900

    javascript:void(0) 제거

--- depth/test.py
+++ depth/test.py
@@ -3,34 +3,51 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import openpyxl
 import time
+import requests
 
 book = openpyxl.Workbook() # openpyxl에 새 시트를 만들고 
 sheet = book.create_sheet()
 
-import requests
+
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 def get_links(url):
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        links = [link.get('href') for link in soup.find_all('a')]
+        links = []
+        try :
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            for a in soup.findAll("a"):
+                try :
+                    href = a['href']
+                    if href.startswith('#') or href.startswith('javascript:') or len(href) == 0:
+                        continue
+                    if href.startswith('//'):
+                        href = "https:" + href
+                    elif href.startswith('/'):
+                        href = url + href
+                    if href.startswith('http') and href not in url:
+                            links.append(href)
+                        
+                except Exception as e:
+                     print("Error1:", e)
+        except Exception as e:
+            print("Error2:", e)
         return links
-    except Exception as e:
-        print("Error:", e)
-        return []
 
 def crawl_site(url, depth):
     if depth == 0:
         return []
 
     links_to_visit = get_links(url)
+    if links_to_visit is None:
+         return []
+    
     child_links = []
 
     for link in links_to_visit:
         abs_link = urljoin(url, link)
-        child_links.append(abs_link)
+        child_links.append((abs_link, depth))
 
         if depth > 1:
             child_links.extend(crawl_site(abs_link, depth - 1))
@@ -38,16 +55,16 @@ def crawl_site(url, depth):
     return child_links
 
 if __name__ == "__main__":
-    start_url = "https://www.starbucks.co.kr/index.do/"  # 시작할 URL
-    depth = 2  # 크롤링 깊이
+    start_url = "https://www.starbucks.co.kr/index.do"  # 시작할 URL
+    depth = 2 # 크롤링 깊이
 
     crawled_links = crawl_site(start_url, depth)
 
     # 결과 출력
     print("Crawled Links:")
-    for link in crawled_links:
-        print(link)
+    for links, depth in crawled_links:
+        print(f"{links} | Depth: {depth}")
 
-        sheet.append([link])
+        sheet.append([links])
     book.save("result.xlsx")
     book.close()
